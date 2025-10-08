84cbe2aa6f1333e3f95ac35ac627207dd5245ce9
Kelvin Santos <kelvinsantosdealmeida@gmail.com>
2025-10-08 17:57:33 -0300

    artigo atualizado e documentado

--- routes/artigo/get_all.py
+++ routes/artigo/get_all.py
@@ -15,38 +15,14 @@ def get_artigo(current_user):
     try:
         cursor = conn.cursor()
 
-        search_artigos = """SELECT artigo.artigo_id, artigo.supervisor_id, artigo.nome_artigo_documento, artigo.conteudo_artigo_digitado, artigo.titulo, artigo.descricao, usu.nome_completo supervisor_nome FROM artigo INNER JOIN supervisor USING(supervisor_id) INNER JOIN usuario usu USING(usuario_id);"""
+        search_artigos = """SELECT artigo.artigo_id, artigo.supervisor_id, artigo.link_artigo, artigo.titulo, artigo.descricao, artigo.imagem_nome, usu.nome_completo supervisor_nome FROM artigo INNER JOIN supervisor USING(supervisor_id) INNER JOIN usuario usu USING(usuario_id);"""
 
         cursor.execute(search_artigos)
         all_artigos = cursor.fetchall()
 
-        arquivos = {}
-        
-        for art in all_artigos:
-            art['arqivos_anexados'] = {
-                "documento_anexado": art['nome_artigo_documento']
-            }
-
+        # return jsonify(all_artigos), 200
+        return f"{all_artigos}", 200
     
-    except Exception as e:
-        conn.rollback()
-        cursor.close()
-        return jsonify({"error": str(e)}), 500
-
-    try:
-
-        # Buscar documento anexado se ouver
-        search_arquivos = """SELECT arquiv.arquivo_artigo_id, arquiv.arquivo_nome, arquiv.artigo_id FROM arquivo_artigo arquiv;"""
-        
-        cursor.execute(search_arquivos)
-        arquivos = cursor.fetchall()
-
-        for art in all_artigos:
-            anexos = [arq for arq in arquivos if arq['artigo_id'] == art['artigo_id']]
-            for anexo in anexos:
-                anexo.pop('artigo_id', None)  # Remove a chave se existir
-            art['arqivos_anexados']['arquivos'] = anexos
-        
     except Exception as e:
         conn.rollback()
         cursor.close()
